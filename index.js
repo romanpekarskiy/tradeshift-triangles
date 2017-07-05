5af77938c8d84b6bdcce150c0537e2efbc9f4ef6
Roman Pekarskiy <roman.pekarskiy@gmail.com>
2017-07-05 14:54:41 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+/**
+ * Returns informarion about triangle
+ * @param {number} lengthA.
+ * @param {number} lengthB.
+ * @param {number} lengthC.
+ * @return {object} triangle type informaton (scalene, equilateral, isosceles).
+ */
+function getTriangleInfo(lengthA, lengthB, lengthC) {
+    if(lengthA === undefined || lengthB === undefined || lengthC === undefined){
+        throw new getTriangleInfo.NotEnoughArgumentsError;
+    }
+
+    var edgeLengths = [lengthA, lengthB, lengthC];
+
+    // check input types
+    edgeLengths.forEach(edgeLength => {
+        if(typeof edgeLength != 'number'){
+            throw new getTriangleInfo.WrongArgumentTypeError;
+        }
+
+		// if one of edges length is zero - all 3 points in one line. This is not a triangle
+        if(edgeLength == 0){
+            throw new getTriangleInfo.ZeroNumberError;
+        }
+
+        if(edgeLength < 0){
+            throw new getTriangleInfo.NegativeNumberError;
+        }
+    })
+
+    // sorting edge lengths from biggest to smallest
+    edgeLengths.sort((a,b) => a-b).reverse();
+
+    // triangle edges must be connected. So we check this is possible.
+    // if sum of 2 smallest edge lengths is smaller than bigger one - we cant connect them
+    if(edgeLengths[0] >= edgeLengths[1] + edgeLengths[2]){
+        throw new getTriangleInfo.WrongEdgeLenghtsError;
+    }
+
+    var triangleInfo = {};
+
+    if(edgeLengths[0] == edgeLengths[1] || edgeLengths[1] == edgeLengths[2]){
+        triangleInfo.isosceles = true;
+    }
+
+    if(edgeLengths[0] == edgeLengths[2]){
+        triangleInfo.equilateral = true;
+    }
+
+    if(!triangleInfo.isosceles && !triangleInfo.equilateral){
+        triangleInfo.scalene = true;
+    }
+
+    return triangleInfo;
+}
+
+getTriangleInfo.NotEnoughArgumentsError = function() {};
+getTriangleInfo.WrongArgumentTypeError = function() {};
+getTriangleInfo.NegativeNumberError = function() {};
+getTriangleInfo.ZeroNumberError = function() {};
+getTriangleInfo.WrongEdgeLenghtsError = function() {};
---
+++ tests.js
@@ -0,0 +1,48 @@
+QUnit.test("Test metdod throws wrong input errors", function( assert ) {
+    assert.throws(() => getTriangleInfo([5, 10, 25]),   getTriangleInfo.NotEnoughArgumentsError);
+    assert.throws(() => getTriangleInfo(5),             getTriangleInfo.NotEnoughArgumentsError);
+    assert.throws(() => getTriangleInfo(5, 5),          getTriangleInfo.NotEnoughArgumentsError);
+
+    assert.throws(() => getTriangleInfo(5, 5, null),    getTriangleInfo.WrongArgumentTypeError);
+    assert.throws(() => getTriangleInfo(5, 5, '5'),     getTriangleInfo.WrongArgumentTypeError);
+    assert.throws(() => getTriangleInfo(5, 5, [5]),     getTriangleInfo.WrongArgumentTypeError);
+    assert.throws(() => getTriangleInfo(5, {}, 5),      getTriangleInfo.WrongArgumentTypeError);
+
+    assert.throws(() => getTriangleInfo(-50, 25, 35),   getTriangleInfo.NegativeNumberError);
+    assert.throws(() => getTriangleInfo(50, -25, 35),   getTriangleInfo.NegativeNumberError);
+    assert.throws(() => getTriangleInfo(50, 25, -35),   getTriangleInfo.NegativeNumberError);
+
+    assert.throws(() => getTriangleInfo(0, 25, 35),     getTriangleInfo.ZeroNumberError);
+    assert.throws(() => getTriangleInfo(25, 0, 35),     getTriangleInfo.ZeroNumberError);
+    assert.throws(() => getTriangleInfo(25, 35, 0),     getTriangleInfo.ZeroNumberError);
+});
+
+QUnit.test("Test metdod throws wrong edge lenghts errors", function( assert ) {
+   assert.throws(() => getTriangleInfo(10, 15, 35),                   getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(10, 15, 55),                   getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(25, 25, 50),                   getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(1, 1, 5),                      getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(Infinity, 1, 5),               getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(Infinity, Infinity, 5),        getTriangleInfo.WrongEdgeLenghtsError);
+   assert.throws(() => getTriangleInfo(Infinity, Infinity, Infinity), getTriangleInfo.WrongEdgeLenghtsError);
+});
+
+QUnit.test("Test triangles", function( assert ) {
+   assert.deepEqual(getTriangleInfo(25, 15, 35),              {scalene: true});
+   assert.deepEqual(getTriangleInfo(25.12, 15.1234, 35.2134), {scalene: true});
+   assert.deepEqual(getTriangleInfo(0.2, 0.3, 0.4),           {scalene: true});
+
+   assert.deepEqual(getTriangleInfo(25, 25, 25),        {isosceles: true, equilateral: true});
+   assert.deepEqual(getTriangleInfo(15, 15, 15),        {isosceles: true, equilateral: true});
+   assert.deepEqual(getTriangleInfo(0.56, 0.56, 0.56),  {isosceles: true, equilateral: true});
+
+   assert.deepEqual(getTriangleInfo(0.56, 0.56, 0.1),   {isosceles: true});
+
+   assert.deepEqual(getTriangleInfo(50, 50, 1),         {isosceles: true});
+   assert.deepEqual(getTriangleInfo(50, 1, 50),         {isosceles: true});
+   assert.deepEqual(getTriangleInfo(1, 50, 50),         {isosceles: true});
+
+   assert.deepEqual(getTriangleInfo(10, 10, 15),        {isosceles: true});
+   assert.deepEqual(getTriangleInfo(10, 15, 10),        {isosceles: true});
+   assert.deepEqual(getTriangleInfo(15, 10, 10),        {isosceles: true});
+});
